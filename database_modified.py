28417a1cc697c988075ec1a2daa2df7e4dcded11
near2size <151147191+near2size@users.noreply.github.com>
2024-04-26 22:22:08 +0700

    Add files via upload

--- database_modified.py
+++ database_modified.py
@@ -2,19 +2,28 @@ import sqlite3
 import random
 
 
-def give_categories():
-    conn = sqlite3.connect('working_base.db')
-    cursor = conn.cursor()
-    cursor.execute(f"""
+def give_categories(s=f"""
     SELECT QuestionTypes.type_name
     FROM QuestionTypes;
-    """)
+    """):
+    conn = sqlite3.connect('working_base.db')
+    cursor = conn.cursor()
+    cursor.execute(s)
     data = cursor.fetchall()
     conn.close()
     return data
 
 
-def give_questions_var(cat, diff=-1):
+def give_variants(type):
+    return list(filter(lambda x: not x.isdigit(), map(lambda x: x[0], give_categories(f"""
+    SELECT Questions.correct_answer
+    FROM Questions
+    JOIN QuestionTypes ON Questions.type_id = QuestionTypes.type_id
+    WHERE QuestionTypes.type_name = '{type}';
+    """))))
+
+
+def give_questions_var(cat=-1, diff=-1):
     big_request = []
     if cat == -1:
         cat = give_categories()
@@ -29,20 +38,24 @@ def give_questions_var(cat, diff=-1):
         """
         cursor.execute(sql_query)
         data = cursor.fetchall()
+
         x = [j for j in range(0, len(data) - 1)]
         random.shuffle(x)
-        for i in x:
-            if not int(data[i][-1]):
-                with open('str_answer.txt', 'r', encoding='utf8') as file:
-                    text = file.readlines()
-                    list_of_uncorrected_answer = [text[random.randint(0, 100)].strip('\n') for i in range(3)]
-                    while data[1] in list_of_uncorrected_answer:
-                        list_of_uncorrected_answer = [text[random.randint(0, 100)].strip('\n') for i in range(3)]
 
-                    request = {'id': data[0],
-                               'text': data[1],
-                               'correct_answer': data[2],
-                               'variants': list_of_uncorrected_answer}
+        for i in x:
+            if diff == 0:
+                break
+            diff -= 1
+            if not int(data[i][-1]):
+                text = give_variants(mini_cat)
+                list_of_uncorrected_answer = [text[random.randint(0, len(text) - 1)].strip('\n') for i in range(3)]
+                while data[1] in list_of_uncorrected_answer:
+                    list_of_uncorrected_answer = [text[random.randint(0, len(text) - 1)].strip('\n') for i in range(3)]
+
+                request = {'id': data[0],
+                           'text': data[1],
+                           'correct_answer': data[2],
+                           'variants': list_of_uncorrected_answer}
             else:
                 corr_int_answer = int(data[i][2])
                 turn = ['+', '-']
@@ -50,7 +63,8 @@ def give_questions_var(cat, diff=-1):
                 for j in range(3):
                     incorrect_answer = eval(f'{corr_int_answer}{turn[random.randint(0, 1)]}{random.randint(0, 100)}')
                     while incorrect_answer <= 0:
-                        incorrect_answer = eval(f'{corr_int_answer}{turn[random.randint(0, 1)]}{random.randint(0, 100)}')
+                        incorrect_answer = eval(
+                            f'{corr_int_answer}{turn[random.randint(0, 1)]}{random.randint(0, 100)}')
                     list_of_incorrect_answer.append(incorrect_answer)
 
                     request = {'id': data[0],
@@ -59,5 +73,10 @@ def give_questions_var(cat, diff=-1):
                                'variants': list_of_incorrect_answer}
 
             big_request.append(request)
+
+
         conn.close()
     return big_request
+
+print(give_categories())
+print(give_questions_var(['Страны и города'], 12))
