35462155df1e9d418f94478f63f95d12832637ae
ViktorGrishin <grishin06Vt@yandex.ru>
2024-05-12 17:46:43 +0700

    commit

--- database.py
+++ database.py
@@ -23,59 +23,53 @@ def give_variants(type):
     """))))
 
 
-def give_questions(cat=-1, diff=-1):
-    big_request = []
-    if cat == -1:
-        cat = give_categories()
-    for mini_cat in cat:
-        conn = sqlite3.connect('working_base.db')
-        cursor = conn.cursor()
-        sql_query = f"""
-        SELECT Questions.question_id, Questions.question_text, Questions.correct_answer, Questions.answer_format
-        FROM Questions
-        JOIN QuestionTypes ON Questions.type_id = QuestionTypes.type_id
-        WHERE QuestionTypes.type_name = "{mini_cat}";
-        """
-        cursor.execute(sql_query)
-        data = cursor.fetchall()
+def give_questions(cat=-1, cant_use=['1000']):
+    cat = cat[0]
+    conn = sqlite3.connect('working_base.db')
+    cursor = conn.cursor()
+    sql_query = f"""
+    SELECT Questions.question_id, Questions.question_text, Questions.type_id, Questions.correct_answer, Questions.answer_format
+    FROM Questions
+    JOIN QuestionTypes ON Questions.type_id = QuestionTypes.type_id
+    WHERE QuestionTypes.type_name = '{cat}'
+        AND Questions.question_id NOT IN ({','.join(list(map(str, cant_use)))})
+    ORDER BY RANDOM()
+    LIMIT 1;
+    """
+    cursor.execute(sql_query)
 
-        x = [j for j in range(0, len(data) - 1)]
-        random.shuffle(x)
+    data = cursor.fetchall()
+    if data:
+        return None
+    data = data[0]
+    if not int(data[-1]):
+        text = give_variants(cat)
+        list_of_uncorrected_answer = [text[random.randint(0, len(text) - 1)].strip('\n') for i in range(3)]
+        while data[3] in list_of_uncorrected_answer:
+            list_of_uncorrected_answer = [text[random.randint(0, len(text) - 1)].strip('\n') for i in range(3)]
 
-        for i in x:
-            if diff == 0:
-                break
-            diff -= 1
-            if not int(data[i][-1]):
-                text = give_variants(mini_cat)
-                list_of_uncorrected_answer = [text[random.randint(0, len(text) - 1)].strip('\n') for i in range(3)]
-                while data[1] in list_of_uncorrected_answer:
-                    list_of_uncorrected_answer = [text[random.randint(0, len(text) - 1)].strip('\n') for i in range(3)]
+        request = {'id': data[0],
+                   'text': data[1],
+                   'correct_answer': data[3],
+                   'variants': list_of_uncorrected_answer}
+    else:
+        corr_int_answer = int(data[2])
+        turn = ['+', '-']
+        list_of_incorrect_answer = []
+        for j in range(3):
+            incorrect_answer = eval(f'{corr_int_answer}{turn[random.randint(0, 1)]}{random.randint(0, 100)}')
+            while incorrect_answer <= 0:
+                incorrect_answer = eval(
+                    f'{corr_int_answer}{turn[random.randint(0, 1)]}{random.randint(0, 100)}')
+            list_of_incorrect_answer.append(incorrect_answer)
 
-                request = {'id': data[i][0],
-                           'text': data[i][1],
-                           'correct_answer': data[i][2],
-                           'variants': list_of_uncorrected_answer}
-            else:
-                corr_int_answer = int(data[i][2])
-                turn = ['+', '-']
-                list_of_incorrect_answer = []
-                for j in range(3):
-                    incorrect_answer = eval(f'{corr_int_answer}{turn[random.randint(0, 1)]}{random.randint(0, 100)}')
-                    while incorrect_answer <= 0:
-                        incorrect_answer = eval(
-                            f'{corr_int_answer}{turn[random.randint(0, 1)]}{random.randint(0, 100)}')
-                    list_of_incorrect_answer.append(incorrect_answer)
+            request = {'id': data[0],
+                       'text': data[1],
+                       'correct_answer': data[3],
+                       'variants': list(map(str, list_of_incorrect_answer))}
 
-                    request = {'id': data[i][0],
-                               'text': data[i][1],
-                               'correct_answer': data[i][2],
-                               'variants': list_of_incorrect_answer}
-
-            big_request.append(request)
+    conn.close()
+    return request
 
 
-        conn.close()
-    return big_request
-
-# print(*give_questions(['Столицы'], diff=3), sep='\n')
+print(*give_questions(['Озера'], cant_use=[93, 94, 95, 96]), sep='\n')
